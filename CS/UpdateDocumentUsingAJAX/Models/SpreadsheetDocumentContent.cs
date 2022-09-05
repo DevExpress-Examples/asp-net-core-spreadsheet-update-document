051cc9c8aa68e02dd7abf279bc4b94f5f8bafbbd
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2022-09-05 16:25:52 +0400

    2

---
+++ CS/UpdateDocumentUsingAJAX/Models/SpreadsheetDocumentContent.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpdateDocumentUsingAJAX.Models
+{
+    public class SpreadsheetDocumentContent
+    {
+        public string DocumentId { get; set; }
+        public Func<byte[]> ContentAccessorByBytes { get; set; }
+        public DocumentFormat DocumentFormat { get; set; } = DocumentFormat.Xlsx;
+
+        public SpreadsheetDocumentContent(string documentId) {
+            DocumentId = documentId;
+        }
+        
+        public SpreadsheetDocumentContent(string documentId, Func<byte[]> contentAccessorByBytes) {
+            DocumentId = documentId;
+            ContentAccessorByBytes = contentAccessorByBytes;
+        }
+
+    }
+}
